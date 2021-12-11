ad47b796d35069a9b160324ea94c8388fb6187e3
nualima <redwan.gharbi@hotmail.com>
2021-12-11 12:22:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        console.log("addTask"), <
+        addTask / > ,
+        console.log("taskList"), <
+        taskList / >
+    );
+}
+
+export default App;
